4f0fde52f4e23c2caee035a4de32688587f25734
Haymaker <haymaker@users.noreply.github.com>
2023-02-25 22:36:17 -0500

    cache data objects added

--- src/Cache/Point.php
+++ src/Cache/Point.php
@@ -2,7 +2,26 @@
 
 namespace Vatradar\Dataobjects\Cache;
 
-class Point
+/**
+ * Simple latitude/longitude coordinate point
+ */
+class Point implements CacheInterface
 {
+    public float $latitude;
+    public float $longitude;
 
+    public function __construct(float $latitude, float $longitude) {
+        $this->latitude  = $latitude;
+        $this->longitude = $longitude;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function array(): array {
+        return [
+            'latitude'  => $this->latitude,
+            'longitude' => $this->longitude,
+        ];
+    }
 }
--- src/Cache/TrackPoint.php
+++ src/Cache/TrackPoint.php
@@ -2,7 +2,30 @@
 
 namespace Vatradar\Dataobjects\Cache;
 
-class TrackPoint
+/**
+ * Defines a Track Point of aeronautical data that includes location,
+ * vector, and altitude information altogether.
+ */
+class TrackPoint implements CacheInterface
 {
+    public string $timestamp;
+    public float  $latitude;
+    public float  $longitude;
+    public int    $altitude;
+    public int    $heading;
+    public int    $groundspeed;
 
+    /**
+     * @inheritDoc
+     */
+    public function array(): array {
+        return [
+            'timestamp'   => $this->timestamp,
+            'latitude'    => $this->latitude,
+            'longitude'   => $this->longitude,
+            'altitude'    => $this->altitude,
+            'heading'     => $this->heading,
+            'groundspeed' => $this->groundspeed,
+        ];
+    }
 }
