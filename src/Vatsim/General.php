4b6ed803643976ca0ed16d63901250f694bc197b
Haymaker <haymaker@users.noreply.github.com>
2023-03-04 04:31:55 -0500

    fix(all): initial monorepo import

---
+++ src/Vatsim/General.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+/*
+ * This file is part of a VATRadar package.
+ *
+ * Copyright (c) 2022 VATRadar <dev@vatradar.net>
+ *
+ * For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Vatradar\Dataobjects\Vatsim;
+
+use DateTimeImmutable;
+
+final class General
+{
+    public function __construct(
+        public readonly int $version,
+        public readonly int $reload,
+        public readonly string $update,
+        public readonly DateTimeImmutable $updateTimestamp,
+        public readonly int $connectedClients,
+        public readonly int $uniqueUsers,
+    ) {
+    }
+}
